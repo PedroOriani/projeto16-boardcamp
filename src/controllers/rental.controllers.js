d7dc8bbce287d832ab5d569ce9a218d0ee74107a
Pedro Sebinelli de Godoy Oriani <TRPEDORI@traumec.com.br>
2023-07-31 16:38:20 -0300

    corrigindo o post rentals

--- src/controllers/rental.controllers.js
+++ src/controllers/rental.controllers.js
@@ -52,7 +52,7 @@ export async function postRental(req,res){
     if(game.rows.length === 0) return res.sendStatus(400);
 
     const gameAvailable = await db.query(`SELECT * FROM rentals WHERE "gameId"=$1;`, [gameId])
-    if(game.rows[0].stockTotal < gameAvailable.rows.length) return res.sendStatus(400);
+    if(game.rows[0].stockTotal <= gameAvailable.rows.length) return res.sendStatus(400);
 
     const pricePerDay = game.rows[0].pricePerDay;
     const rentDate = format(new Date(), 'yyyy-MM-dd');
@@ -76,13 +76,26 @@ export async function finalizeRental(req,res){
 
     try{
         const rental = await db.query('SELECT * FROM rentals WHERE id=$1;', [id])
-        if (rental === null) return res.sendStatus(404)
+
+        if (rental.rows.length === 0) return res.sendStatus(404)
         if (rental.rows[0].returnDate != null) return res.sendStatus(400)
 
+        const finalDay = format(addDays(rental.rentDate, rental.daysRented), 'yyyy-MM-dd')
+        const perDay = rental.rentDate / rental.daysRented
+        const fee = difDias(finalDay, returnDate) > 0 ? difDias(finalDay, returnDate) * perDay : 0;
 
     }catch (err){
         res.status(500).send(err.messsage)
     }
+}
+
+function difDias(d1, d2){
+    const data1 = new Date(d1)
+    const data2 = new Date(d2)
+
+    const dif = data2 - data1
+    const dias = dif / (1000 * 60 * 60 * 24);
+    return dias 
 }
 
 export async function deleteRental(req,res){
