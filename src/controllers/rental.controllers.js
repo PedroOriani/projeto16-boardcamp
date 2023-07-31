5b810a790b9b33e62f091e8383314e7684cd0595
Pedro Sebinelli de Godoy Oriani <TRPEDORI@traumec.com.br>
2023-07-31 14:47:34 -0300

    post rentals

--- src/controllers/rental.controllers.js
+++ src/controllers/rental.controllers.js
@@ -20,16 +20,31 @@ export async function postRental(req,res){
     const { customerId, gameId, daysRented } = req.body
 
     try{
+    
+    daysRented = Number(daysRented);
+    if(daysRented <= 0) return res.sendStatus(400);
 
-    const game = await db.query(`SELECT * FROM games WHERE id=$1`, [gameId]);
+    const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [customerId]);
+    if(!customer) return res.sendStatus(400);
+
+    const game = await db.query(`SELECT * FROM games WHERE id=$1;`, [gameId]);
+    if(!game) return res.sendStatus(400);
+
+    if(game.stockTotal < 1) return res.sendStatus(400);
+
     const pricePerDay = game.rows[0].pricePerDay;
+    const rentDate = format(new Date(), 'yyyy-MM-dd');
+    const returnDate = null;
+    const delayFee = null;
+    const originalPrice = daysRented * pricePerDay;
 
-    daysRented = Number(daysRented)
-    const rentDate = format(new Date(), 'yyyy-MM-dd')
-    const returnDate = null
-    const delayFee = null
-    const originalPrice = daysRented * pricePerDay
-        
+    const rental = await db.query(`
+    INSERT INTO rentals
+    ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFree")
+    VALUES ($1, $1, $3, $4, $5, $6, $7)`,
+    [customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee]
+    );
+        res.sendStatus(201)
     }catch (err){
         res.status(500).send(err.messsage)
     }
