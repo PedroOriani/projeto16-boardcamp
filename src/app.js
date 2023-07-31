1904e2d2c2c4673f6ccf572d42055897a1cb62d1
Pedro Sebinelli de Godoy Oriani <TRPEDORI@traumec.com.br>
2023-07-31 08:13:47 -0300

    arrumando database connection

--- src/app.js
+++ src/app.js
@@ -8,4 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router)
 
-app.listen(process.env.PORT || 5000);
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,4 +1,4 @@
-import pg, { Pool } from 'pg';
+import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
