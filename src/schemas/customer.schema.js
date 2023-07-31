214342c1392654f22a287b252e25ea27308c4079
Pedro Oriani <pedro.oriani@gmail.com>
2023-07-31 12:13:18 -0300

    arrumando schema do CPF, limitando a numeros

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 const customerSchema = Joi.object({
     name: Joi.string().min(1).required(),
     phone: Joi.string().min(10).max(11).required(),
-    cpf: Joi.string().length(11).required(),
+    cpf: Joi.string().regex(/^[0-9]{11}$/).length(11).required(),
     birthday: Joi.date().required(),
 });
 
