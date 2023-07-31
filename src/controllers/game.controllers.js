c9178222dbab6294c9f6084133abb64e898ba543
Pedro Sebinelli de Godoy Oriani <TRPEDORI@traumec.com.br>
2023-07-31 09:14:56 -0300

    post jogos

--- src/controllers/game.controllers.js
+++ src/controllers/game.controllers.js
@@ -2,7 +2,7 @@ import { db } from '../database/database.connection.js'
 
 export async function getGames(req,res){
     try{
-        const games = await db.query(`SELECT * FROM games;`);
+        const games = await db.query('SELECT * FROM games;');
         res.send(games.rows)
     }catch (err){
         res.status(500).send(err.message)
@@ -11,7 +11,15 @@ export async function getGames(req,res){
 
 export async function postGame(req,res){
     try{
-        
+        const name = await db.query('SELECT * FROM games WHERE name=$1', [req.body.name]);
+
+        if (name) return res.status(409).send('Esse jogo ja está cadastrado');
+        await db.query(
+            'INSERT INTO games (name, image, "stockTotal", "pricePerDay" VALUES ($1, $2, $3, $4)',
+            [req.body.name, req.body.image, req.body.stockTotal, req.body.pricePerDay]
+        )
+
+        res.sendStatus(201)
     }catch (err){
         res.status(500).send(err.message)
     }
